278fcfc97caca410d44671df9066c14cf29db98f
CozyCrypto <fassdavid722@gmail.com>
2026-07-11 01:19:47 +0100

    feat: scaffold

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './types.js';
+export { ShellEngine } from './Shell/shell.js';
+export { CommandLibrary } from './Commands/commands.js';
+export { ProcessManager } from './Processes/processes.js';
+export { BuildEngine } from './Build/build.js';
+export { PackageManager } from './Packages/packages.js';
+export { LogViewer } from './Logs/logs.js';
+export { EnvironmentManager } from './Environment/env.js';
